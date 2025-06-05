aa703a7a72ea426d3fa9a7eb46cf4bc7cc2b5f73
Chriishero <chris.vill@icloud.com>
2025-06-05 21:02:56 +0200

    Double Pendulum Simulation (C++)
    Équation des accélérations potentiellement corrigé ?

--- C-C++/Project/SFML/Double Rod Physics Simulation/World.cpp
+++ C-C++/Project/SFML/Double Rod Physics Simulation/World.cpp
@@ -29,32 +29,32 @@ void World::motion() {
 	float m2 = m_vecRod[1]->getm_weight();
 	float l1 = m_vecRod[0]->getm_size().y / 100.0f; // normalisation des longueurs : 250 pixels = 2.5 m
 	float l2 = m_vecRod[1]->getm_size().y / 100.0f; // normalisation des longueurs : 250 pixels = 2.5 m
+	float g = m_gravity;
 
 	float kineticEnergy1 = 0.5f  * m1 * dtheta1 * dtheta1 * l1 * l1;
 	m_vecRod[0]->setm_kineticEnergy(kineticEnergy1);
-	float potentialEnergy1 = -m1 * m_gravity * l1 * cos(theta1);
+	float potentialEnergy1 = -m1 * g * l1 * cos(theta1);
 	m_vecRod[0]->setm_potentialEnergy(potentialEnergy1);
 
 	float kineticEnergy2 = 0.5f * m2 * (dtheta1 * dtheta1 * l1 * l1 + 2 * dtheta1 * l1 * dtheta2 * l2 * cos(theta1 - theta2)
 							+ dtheta2 * dtheta2 * l2 * l2);
 	m_vecRod[1]->setm_kineticEnergy(kineticEnergy2);
-	float potentialEnergy2 = -m2 * m_gravity * (l1 * cos(theta1) + l2 * cos(theta2));
+	float potentialEnergy2 = -m2 * g * (l1 * cos(theta1) + l2 * cos(theta2));
 
 	m_energy = kineticEnergy1 + kineticEnergy2 + potentialEnergy1 + potentialEnergy2;
 
 	float a = (m1 + m2) * l1 * l1;
-	float b = m2 * l2 * l2 * cos(theta1 - theta2);
+	float b = m2 * l1 * l2 * cos(theta1 - theta2);
 	float c = b;
 	float d = m2 * l2 * l2;
-	float f = -m2 * dtheta1 * l1 * dtheta2 * l2 * sin(theta1 - theta2) - (m1 + m2) * m_gravity * l1 * sin(theta1) +
-		m1 * l1 * l2 * dtheta2 * sin(theta1 - theta2) * (dtheta1 - dtheta2);
-	float g = m2 * dtheta1 * l1 * dtheta2 * l2 * sin(theta1 - theta2) - m2 * m_gravity * l2 * sin(theta2) - m2 * l1 * l2 *
-		dtheta1 * sin(theta1 - theta2) * (dtheta1 - dtheta2);
+	float e = -(m1 + m2) * g * l1 * sin(theta1) - m2 * l1 * l2 * dtheta2 * dtheta2 * sin(theta1 - theta2);
+	float f = -m2 * g * l2 * sin(theta2) + m2 * l1 * l2 * dtheta1 * dtheta1 * sin(theta1 - theta2);
 
-	double ddtheta1 = (f * d - g * b) / (a * d - b * c);
+
+	double ddtheta1 = (e * d - f * b) / (a * d - b * c);
 	m_vecRod[0]->setm_angularAcceleration(ddtheta1);
-
-	double ddtheta2 = (g * a - f * c) / (a * d - b * c);
+	
+	double ddtheta2 = (f * a - e * c) / (a * d - b * c);
 	m_vecRod[1]->setm_angularAcceleration(ddtheta2);
 }
 

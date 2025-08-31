8afc878522089e1d1a5b9d28e5bb6c319358ba6f
Chriishero <chris.vill@icloud.com>
2025-08-31 21:45:21 +0200

    Gaussian Disciminant Analysis (Python)
    - Ajout des fonctions means, prior proba et covariance matrix, celle sur la matrice de covariance semble encore fausse

--- Python/Intelligence Artificielle/Projet/Model/gda.py
+++ Python/Intelligence Artificielle/Projet/Model/gda.py
@@ -4,27 +4,41 @@ class GDA:
     def __init__(self):
         self.phi, self.mu, self.sigma = None, None, None
 
-    def _prior_probability(self, y):
+    def _prior_probability(self, y, classe):
         m = len(y)
-        return 1/m * np.sum(y)
+        return 1/m * np.sum(y == classe)
     
-    def _means_function(self, X, y):
-        m, n = X.shape
-        classes = np.unique(y)
-        return np.array([np.sum(X[c == y]) / np.sum(np.ones_like(X)[c == y]) for c in classes])
+    def _mean(self, X, y, classe):
+        m_c = len(y[classe])
+        return 1/m_c * np.sum(X[y == classe], axis=0)
     
-    def _covariance_matrix(self, X, y):
+    def _covariance_matrix(self, X, y, classe):
         m, n = X.shape
-        cov = np.zeros((2, 2))
-        cov[0, 0] = 1/m * np.sum((X - np.mean(X))**2)
-        cov[0, 1] = 1/m * np.sum((np.sum(X) - np.mean(X)) * (np.sum(y) - np.mean(y)))
-        cov[1, 0] = cov[0, 1]
-        cov[1, 1] = 1/m * np.sum((y - np.mean(y))**2)
-        return cov
+        return 1/m * np.sum(X - self._mean(X, y, classe)**2, axis=0)
+
+    def fit(self, X, y):
+        X, y = np.array(X), np.array(y)
+        m, n, k = X.shape[0], X.shape[1], np.max(y) + 1
+        self.phi = np.zeros(k)
+        self.mu = np.zeros((k, n))
+        self.sigma = np.zeros((k, n, n))
+
+        for c in range(k):
+            self.phi[c] = self._prior_probability(y, c)
+            self.mu[c] = self._mean(X, y, c)
+            self.sigma[c] = self._covariance_matrix(X, y, c)
+
+    def _bayes_rules(self, X):
+        pass
+    
+    def predict(self, X):
+        return np.argmax(self._bayes_rules(np.array(X)), axis=1)
 
 from sklearn.datasets import make_classification
 X, y = make_classification(n_samples=500, n_features=10, n_informative=8, n_classes=2, random_state=0)
 
 model = GDA()
-print(model._means_function(X, y))
-print(model._covariance_matrix(X, y))
+model.fit(X, y)
+print(model.phi)
+print(model.mu)
+print(model.sigma)
